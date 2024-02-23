8d9362555ecddf8e5410fcb1c44f4704a196c0ab
Hung Trinh <141187458+Nagacool123@users.noreply.github.com>
2024-02-23 17:24:52 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Signup from "./signup.js";
 import ProfileForm from "./profile.js";
 import Homepage from "./homepage.js";
 import Navbar from "./navbar.js";
+import Quote from "./fuelQuoteForm.js";
 import FuelQuoteHistory from "./fuelQuoteHistory.js";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -13,17 +14,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="bg-[#FBFAF5]">
-        <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/profile" element={<ProfileForm />}></Route>
-        <Route path="/homepage" element={<Homepage />}></Route>
-        <Route path="/navbar" element={<Navbar />}></Route>
-        <Route path="/fuel" element={<FuelQuoteHistory/>}></Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/profile" element={<ProfileForm />}></Route>
+          <Route path="/homepage" element={<Homepage />}></Route>
+          <Route path="/navbar" element={<Navbar />}></Route>
+          <Route path="/fuelQuoteForm" element={<Quote />}></Route>
+          <Route path="/fuel" element={<FuelQuoteHistory />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
+
+    // <div className="">
+    //    <Login />
+    // </div>
   );
 }
 
--- src/navbar.js
+++ src/navbar.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdHistory } from "react-icons/md";
 import { BiHome } from "react-icons/bi";
 import { CgProfile } from "react-icons/cg";
 import { FaWpforms } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState(location.pathname);
+
+  const handleLinkClick = (path) => {
+    setActiveLink(path);
+  };
+
   return (
-    <div className="bg-[#061013] text-[#FBFAF5] font-montserrat font-bold text-lg flex flex-row justify-evenly py-4 xl:h-screen xl:w-fit xl:p-6 xl:flex-col xl:gap-y-12 xl:absolute">
-      <NavLink className="px-4" to="/profile">
+    <div className="bg-[#153640] text-[#FBFAF5] font-montserrat font-bold text-lg flex flex-row justify-evenly py-4 xl:h-screen xl:w-fit xl:p-6 xl:flex-col xl:gap-y-12">
+      <Link
+        to="/profile"
+        className={`hover:bg-[#88BBC8] rounded-xl p-1 ${
+          activeLink === "/profile" ? "bg-[#88BBC8]" : ""
+        }`}
+        onClick={() => handleLinkClick("/profile")}
+      >
         <CgProfile />
-      </NavLink>
-      <NavLink className="px-4" to="/">
+      </Link>
+      <Link
+        to="/"
+        className={`hover:bg-[#88BBC8] rounded-xl p-1 ${
+          activeLink === "/" ? "bg-[#88BBC8]" : ""
+        }`}
+        onClick={() => handleLinkClick("/")}
+      >
         <BiHome />
-      </NavLink>
-      <NavLink className="px-4" to="/fuelQuoteForm">
+      </Link>
+      <Link
+        to="/fuelQuoteForm"
+        className={`hover:bg-[#88BBC8] rounded-xl p-1 ${
+          activeLink === "/fuelQuoteForm" ? "bg-[#88BBC8]" : ""
+        }`}
+        onClick={() => handleLinkClick("/fuelQuoteForm")}
+      >
         <FaWpforms />
-      </NavLink>
-      <NavLink className="px-4" to="/fuel">
+      </Link>
+      <Link
+        to="/fuel"
+        className={`hover:bg-[#88BBC8] rounded-xl p-1 ${
+          activeLink === "/fuel" ? "bg-[#88BBC8]" : ""
+        }`}
+        onClick={() => handleLinkClick("/fuel")}
+      >
         <MdHistory />
-      </NavLink>
+      </Link>
     </div>
   );
 }
