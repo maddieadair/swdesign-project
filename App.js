c032098381a0050f434414f5bf2b48cc3ae60dfb
Hung Trinh <141187458+Nagacool123@users.noreply.github.com>
2024-02-23 17:16:50 -0600

    Update App.js

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import ProfileForm from "./profile.js";
 import Homepage from "./homepage.js";
 import Navbar from "./navbar.js";
 import Quote from "./fuelQuoteForm.js";
+import FuelQuoteHistory from "./fuelQuoteHistory.js";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
